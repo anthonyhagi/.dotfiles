7ea6ed3761508e0a5d16f9154b16ef766d046e84
Anthony Hagidimitriou <info@anthonyhagi.com.au>
2024-03-04 14:09:35 +1030

    feat: add custom plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -610,7 +610,7 @@ require("lazy").setup({
 	{ -- Autoformat
 		"stevearc/conform.nvim",
 		opts = {
-			notify_on_error = false,
+			notify_on_error = true,
 			format_on_save = {
 				timeout_ms = 500,
 				lsp_fallback = true,
@@ -819,7 +819,7 @@ require("lazy").setup({
 	--
 	--  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
 	--    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-	-- { import = 'custom.plugins' },
+	{ import = "custom.plugins" },
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`

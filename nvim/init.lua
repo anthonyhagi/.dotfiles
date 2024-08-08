bc21a27815733872ad06c7589e28d52ba5caa3be
Anthony Hagidimitriou <info@anthonyhagi.com.au>
2024-08-08 16:30:43 +0930

    fix: update which-key key bindings to resolve error on startup

--- nvim/init.lua
+++ nvim/init.lua
@@ -269,15 +269,15 @@ require("lazy").setup({
 		},
 	},
 
-	-- NOTE: Plugins can also be configured to run lua code when they are loaded.
+	-- NOTE: Plugins can also be configured to run Lua code when they are loaded.
 	--
 	-- This is often very useful to both group configuration, as well as handle
 	-- lazy loading plugins that don't need to be loaded immediately at startup.
 	--
 	-- For example, in the following configuration, we use:
-	--  event = 'VeryLazy'
+	--  event = 'VimEnter'
 	--
-	-- which loads which-key after all the UI elements are loaded. Events can be
+	-- which loads which-key before all the UI elements are loaded. Events can be
 	-- normal autocommands events (`:help autocmd-events`).
 	--
 	-- Then, because we use the `config` key, the configuration only runs
@@ -286,17 +286,18 @@ require("lazy").setup({
 
 	{ -- Useful plugin to show you pending keybinds.
 		"folke/which-key.nvim",
-		event = "VeryLazy", -- Sets the loading event to 'VeryLazy'
+		event = "VimEnter", -- Sets the loading event to 'VimEnter'
 		config = function() -- This is the function that runs, AFTER loading
 			require("which-key").setup()
 
 			-- Document existing key chains
-			require("which-key").register({
-				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+			require("which-key").add({
+				{ "<leader>c", group = "[C]ode" },
+				{ "<leader>d", group = "[D]ocument" },
+				{ "<leader>r", group = "[R]ename" },
+				{ "<leader>s", group = "[S]earch" },
+				{ "<leader>w", group = "[W]orkspace" },
+				{ "<leader>t", group = "[T]oggle" },
 			})
 		end,
 	},
